36e9e62f745ee830c10dcfbf28071197a0b7d436
eugene.jadeszko <zhenia04.94@mail.ru>
2017-07-12 15:53:52 +0300

    Craeted config classes for RabbitMQ and CouchBase

--- src/main/java/by/intexsoft/config/CouchBaseConfig.java
+++ src/main/java/by/intexsoft/config/CouchBaseConfig.java
@@ -1,5 +1,29 @@
 package by.intexsoft.config;
 
-public class CouchBaseConfig {
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.couchbase.config.AbstractCouchbaseConfiguration;
+import org.springframework.data.couchbase.repository.config.EnableCouchbaseRepositories;
+
+@Configuration
+@EnableCouchbaseRepositories
+public class CouchBaseConfig extends AbstractCouchbaseConfiguration {
+
+	@Override
+	protected List<String> getBootstrapHosts() {
+		return Arrays.asList("couchbase://127.0.0.1");
+	}
+
+	@Override
+	protected String getBucketName() {
+		return "default";
+	}
+
+	@Override
+	protected String getBucketPassword() {
+		return "";
+	}
 
 }
