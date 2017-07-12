48322dd1e30cf1d6af446f9b97c2e8bb0d600b15
eugene.jadeszko <zhenia04.94@mail.ru>
2017-07-12 16:27:26 +0300

    Create QueryWriter class

--- src/main/java/by/intexsoft/service/writer/QueueWriter.java
+++ src/main/java/by/intexsoft/service/writer/QueueWriter.java
@@ -1,5 +1,22 @@
 package by.intexsoft.service.writer;
 
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import by.intexsoft.document.SampleDoc;
+import by.intexsoft.document.BuilderSampleDocImpl;
+
+@Controller
 public class QueueWriter {
 
+	@Autowired
+	RabbitTemplate template;
+	
+	@Autowired
+	SampleDoc sampleDoc;
+
+	private void writeMessage() {
+		template.convertAndSend("message", sampleDoc.builder);
+	}
 }
