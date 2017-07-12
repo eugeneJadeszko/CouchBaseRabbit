239d453e08a3886b478e71b22d038d1303ef610f
Vladislav <grassokvp@gmail.com>
2017-07-12 15:43:03 +0300

    Created SampleDoc and Builder classes

--- src/main/java/by/intexsoft/document/BuilderSampleDoc.java
+++ src/main/java/by/intexsoft/document/BuilderSampleDoc.java
@@ -1,5 +1,23 @@
 package by.intexsoft.document;
 
+import java.util.Random;
+
+/**
+ * This class will build SampleDoc with unique fields.
+ */
 public class BuilderSampleDoc {
+
+    private static int counter = 0;
+    private String randomType = String.valueOf(new Random().nextInt(2));
+
+    /**
+     * Build unique SampleDoc object
+     *
+     * @return unique SampleDoc object
+     */
+    public SampleDoc builder() {
+        counter++;
+        return new SampleDocImpl(counter, randomType, "Ivan" + counter, "Ivanov" + counter, 4 + counter);
+    }
 
 }
