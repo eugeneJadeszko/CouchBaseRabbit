81d0bcd8ee6e1bf6a590d8e1a224b663675d1abf
eugene.jadeszko <zhenia04.94@mail.ru>
2017-07-13 12:36:51 +0300

    add QueueListener

--- src/main/java/by/intexsoft/Runner.java
+++ src/main/java/by/intexsoft/Runner.java
@@ -1,13 +1,16 @@
 package by.intexsoft;
 
-/**
- * Hello world!
- *
- */
-public class Runner 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import org.springframework.context.annotation.ComponentScan;
+
+import by.intexsoft.service.writer.QueueWriter;
+
+@ComponentScan
+public class Runner {
+	public static void main(String[] args) {
+		QueueWriter writer = new QueueWriter();
+		System.out.println("Hello World!");
+		for (int i = 0; i < 5; i++) {
+		writer.writeMessage();	
+		}
+	}
 }
--- src/main/java/by/intexsoft/service/reader/QueueListener.java
+++ src/main/java/by/intexsoft/service/reader/QueueListener.java
@@ -1,5 +1,25 @@
 package by.intexsoft.service.reader;
 
+import java.io.IOException;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import by.intexsoft.document.SampleDoc;
+
 public class QueueListener {
 
+	@Autowired
+	ObjectMapper mapper;
+
+	@RabbitListener(queues = "queue")
+	public void listenQueue(String messageJString) throws JsonParseException, JsonMappingException, IOException {
+		SampleDoc doc = mapper.readValue(messageJString, SampleDoc.class);
+		System.out.println(doc.getId() + " " + doc.getFirstName() + " " + doc.getSecondName() + " " + doc.getAge() + " "
+				+ doc.getType());
+	}
 }
