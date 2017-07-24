afc419b406b709ebde4ca2af52c2569293321ea7
Vladislav <grassokvp@gmail.com>
2017-07-24 14:06:27 +0300

    Modify CallRecordBuilder

--- src/by/intexsoft/billing/service/impl/CallRecordBuilderImpl.java
+++ src/by/intexsoft/billing/service/impl/CallRecordBuilderImpl.java
@@ -1,7 +1,9 @@
 package by.intexsoft.billing.service.impl;
 
 import by.intexsoft.billing.model.CallRecord;
+import by.intexsoft.billing.service.Calculator;
 import by.intexsoft.billing.service.CallRecordBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Random;
@@ -13,8 +15,25 @@ import java.util.Random;
 public class CallRecordBuilderImpl implements CallRecordBuilder {
 
     private static int counter = 1;
-    private int duration = new Random().nextInt(1000000);
-    private boolean type = new Random().nextBoolean();
+    private int duration;
+    private boolean type;
+    private final Calculator calculator;
+    private int subscriberId;
+    private long startTime;
+    private long endTime;
+    private double price;
+
+
+    @Autowired
+    public CallRecordBuilderImpl(CalculatorImpl calculator) {
+        this.calculator = calculator;
+        price = calculator.calculatePriceSingleCall(type, duration);
+        duration = new Random().nextInt(1000000);
+        type = new Random().nextBoolean();
+        subscriberId = new Random().nextInt(21);
+        startTime = System.currentTimeMillis();
+        endTime = startTime + duration;
+    }
 
     /**
      * Build unique {@link CallRecord} object
@@ -23,6 +42,6 @@ public class CallRecordBuilderImpl implements CallRecordBuilder {
      */
     public CallRecord build() {
         counter++;
-        return new CallRecord(counter, duration, type);
+        return new CallRecord(counter, subscriberId, startTime, endTime, duration, type, price);
     }
 }
