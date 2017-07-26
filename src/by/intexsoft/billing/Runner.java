c2a1191d5aeaa9be45cecfdfbc05ade6f0eb2a84
eugene.jadeszko <zhenia04.94@mail.ru>
2017-07-26 21:28:44 +0300

    add sheduled

--- src/by/intexsoft/billing/Runner.java
+++ src/by/intexsoft/billing/Runner.java
@@ -1,15 +1,13 @@
 package by.intexsoft.billing;
 
-import by.intexsoft.billing.service.QueueWriter;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-public class Runner
-{
-    public static void main(String[] args)
-    {
-        ApplicationContext context = new AnnotationConfigApplicationContext("by.intexsoft.billing.config");
-        QueueWriter queueWriter = (QueueWriter) context.getBean("queueWriter");
-        queueWriter.writeMessage();
-    }
+public class Runner {
+	public static void main(String[] args) {
+		ApplicationContext context = new AnnotationConfigApplicationContext("by.intexsoft.billing.config");
+		while (true) {
+
+		}
+	}
 }
